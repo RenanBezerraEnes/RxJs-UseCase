b7d93d9d00fabc176335690e859711792f05d12f
RenanBezerraEnes <renanbezerraenes@icloud.com>
2023-08-21 15:02:23 +0200

    Calling the get service to print out in the html the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CountryModel } from './models/Country.Model';
+import { CountriesService } from './services/countries.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'rxjs-usecase';
+export class AppComponent implements OnInit {
+  countries$!: Observable<CountryModel[]>;
+
+  constructor(private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countries$ = this.countriesService.get();
+  }
 }
